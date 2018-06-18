eb85ebe60bcf0c703f088b7a9e82bdef3a865a4b
Ryan Helgerson <ryan.helgerson@gmail.com>
2018-06-18 17:29:56 -0500

    CRUD methods established in orm and models.

---
+++ models/burger.js
@@ -0,0 +1,34 @@
+//import the ORM
+var orm = require("../config/orm.js")
+
+//Create code to invoke orm methods
+
+var burger = {
+    //Select all of the burgers 
+    selectAll: function(callBack){
+        orm.selectAll(function(res){
+            callBack(res);
+        });
+    },
+
+    //Add a new burger to the db
+    insertOne: function(newBurg, callBack){
+        orm.insertOne(newBurg, function(res){
+            callBack(res);
+        });
+    },
+
+    updateOne: function(eatenBurg, callBack){
+        orm.updateOne(eatenBurg, function(res){
+            callBack(res);
+        });
+    },
+
+    updateOne: function(deletedBurg, callBack){
+        orm.updateOne(deletedBurg, function(res){
+            callBack(res);
+        });
+    }
+}
+
+module.exports = burger;
